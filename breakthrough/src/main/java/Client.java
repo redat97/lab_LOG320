4d7e374c4cf06b4d50c5faf0265ca1c29974c89e
redat97 <flyha@hotmail.ca>
2023-06-15 16:52:11 -0400

    removed board class

--- breakthrough/src/main/java/Client.java
+++ breakthrough/src/main/java/Client.java
@@ -12,6 +12,8 @@ class Client {
         BufferedOutputStream output;
         int[][] board = new int[8][8];
 
+
+
         try {
             MyClient = new Socket("localhost", 8888);
 
@@ -20,7 +22,6 @@ class Client {
             BufferedReader console = new BufferedReader(new InputStreamReader(System.in));
             while(1 == 1){
                 char cmd = 0;
-
                 cmd = (char)input.read();
                 System.out.println(cmd);
                 // Debut de la partie en joueur blanc
@@ -45,10 +46,9 @@ class Client {
                     }
 
                     System.out.println("Nouvelle partie! Vous jouer blanc, entrez votre premier coup : ");
-                    int random_int = (int)Math.floor(Math.random() * (22 - 1 + 1) + 22);
-                    ArrayList<String> possibleMoves = getPossibleMoves(createBoard(s), "2");
+                    //ArrayList<String> possibleMoves = getPossibleMoves(createBoard(s), String.valueOf(player));
                     String move = null;
-                    move = possibleMoves.get(5);
+                    //move = possibleMoves.get(5);
                     output.write(move.getBytes(),0,move.length());
                     output.flush();
                 }
@@ -62,9 +62,9 @@ class Client {
                     input.read(aBuffer,0,size);
                     String s = new String(aBuffer).trim();
                     System.out.println(s);
-                    getPossibleMoves(createBoard(s), "4");
-                    String[] boardValues;
-                    boardValues = s.split(" ");
+                    //getPossibleMoves(createBoard(s), String.valueOf(player));
+                    //String[] boardValues;
+                    /*boardValues = s.split(" ");
                     int x=0,y=0;
                     for(int i=0; i<boardValues.length;i++){
                         board[x][y] = Integer.parseInt(boardValues[i]);
@@ -73,7 +73,7 @@ class Client {
                             x = 0;
                             y++;
                         }
-                    }
+                    }*/
                 }
 
 
@@ -125,41 +125,7 @@ class Client {
 
     }
 
-    private static String[][] createBoard(String s) {
-        s = s.trim();
-        String[][] board = new String[8][8];
-        int index = 0;
-        for (int i = 0; i < 8; i++) {
-            for (int j = 0; j < 8; j++) {
-                board[i][j] = String.valueOf(s.charAt(index));
-                index+=2;
-            }
-        }
-        return board;
-    }
 
-    private static String translate(int i, int j){
-        StringBuilder s = new StringBuilder();
-        if (j == 0) {
-            s.append("A");
-        } else if (j == 1) {
-            s.append("B");
-        } else if (j == 2) {
-            s.append("C");
-        } else if (j == 3) {
-            s.append("D");
-        } else if (j == 4) {
-            s.append("E");
-        } else if (j == 5) {
-            s.append("F");
-        } else if (j == 6) {
-            s.append("G");
-        } else if (j == 7) {
-            s.append("H");
-        }
-
-        return String.valueOf(s.append(i+1));
-    }
     private static ArrayList<String> getPossibleMoves(String[][] board, String player) {
         ArrayList<String> possibleMoves = new ArrayList<>();
         if (player.equals("4"))
@@ -185,15 +151,15 @@ class Client {
                 for (int j = 0; j < 8; j++) {
                     if (board[i][j].equals(player)) {
                         if ((i != 7) && (board[i + 1][j].equals("0"))) {
-                            possibleMoves.add(translate(i, j) + "-" + translate(i + 1, j));
+                            possibleMoves.add(board[i][j] + "-" + board[i+1][j]);
                         }
 
                         if ((i != 0 & j != 7) && !(board[i + 1][j + 1].equals(player))) {
-                            possibleMoves.add(translate(i, j) + "-" + translate(i + 1, j + 1));
+                            possibleMoves.add(board[i][j] + "-" + board[i+1][j+1]);
                         }
 
                         if ((i != 0 & j != 0) && !(board[i + 1][j - 1].equals(player))) {
-                            possibleMoves.add(translate(i, j) + "-" + translate(i + 1, j - 1));
+                            possibleMoves.add(board[i][j] + "-" + board[i+1][j-1]);
                         }
                     }
                 }
@@ -202,10 +168,48 @@ class Client {
         return possibleMoves;
     }
 
-    public void play(String move, String board){
+
+    /*public int evaluate(String [][] board, String player){
+        String opponent;
+        if (player.equals("2")){
+            opponent = "4";
+            for (int i = 0; i < 8; i++) {
+                if (board[7][i].equals(player)){
+                    return 100;
+                }
+
+                if (board[0][i].equals(opponent)){
+                    return -100;
+                }
+            }
+        }else {
+            opponent = "2";
+            for (int i = 0; i < 8; i++) {
+                if (board[7][i].equals(opponent)){
+                    return -100;
+                }
+
+                if (board[0][i].equals(player)){
+                    return 100;
+                }
+            }
+        }
+        return 0;
+    }
+
+    public void play(String move, String[][] board){
         String startingPosition = move.split("-")[0];
         String destinationPosition = move.split("-")[1];
 
-
+        board[destinationPosition.charAt(0)][destinationPosition.charAt(1)] = board[startingPosition.charAt(0)][startingPosition.charAt(1)];
+        board[startingPosition.charAt(0)][startingPosition.charAt(1)] = "0";
     }
+
+    public void undo(String move, String[][] board){
+        String startingPosition = move.split("-")[1];
+        String destinationPosition = move.split("-")[0];
+
+        board[destinationPosition.charAt(1)][destinationPosition.charAt(0)] = board[startingPosition.charAt(1)][startingPosition.charAt(0)];
+        board[startingPosition.charAt(0)][startingPosition.charAt(1)] = "0";
+    }*/
 }
